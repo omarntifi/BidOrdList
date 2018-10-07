85b3dec783106aedd09b8e34dca38cc39e6d9a03
omar <omarntifi1@gmail.com>
2018-10-07 19:25:26 +0200

    Feat: Types and headers of llista.h defined
    Code of llista.h has been modified. Types Node and BidOrdList have been
    created. The headers of the llista.c functions and procedures have also been created.

--- llista.h
+++ llista.h
@@ -7,3 +7,41 @@
 * @ Date of last modification: 7/10/2018
 *
 ***********************/
+
+
+
+#include <studio.h>
+
+typedef struct n {
+    int i;
+    struct n *next;
+    struct n *prev;
+}Node;
+
+typedef struct {
+    Node *start;
+    Node *final;
+    Node *pdi;
+}BidOrdList;
+
+BidOrdList BidOrdList_create();
+
+void BIDORDLIST_insertOrdered(BidOrdList *l, int i);
+
+int BIDORDLIST_consultation(BidOrdList l);
+
+void BIDORDLIST_advance(BidOrdList *l);
+
+void BIDORDLIST_goBack(BidOrdList *l);
+
+void BIDORDLIST_goStart(BidOrdList *l);
+
+void BIDORDLIST_goFinal(BidOrdList *l);
+
+void BIDORDLIST_delete(BidOrdList *l);
+
+int BIDORDLIST_final(BidOrdList l);
+
+int BIDORDLIST_empty(BidOrdList l);
+
+void BIDORDLIST_destroy(BidOrdList *l);
